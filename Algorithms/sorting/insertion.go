ff7d943eddeb25affe50e6a3e538530883aed145
Balaji Srinivasan <balaji.srinivasan@grabtaxi.com>
2018-10-26 15:32:32 +0530

    sorting

--- Algorithms/sorting/insertion.go
+++ Algorithms/sorting/insertion.go
@@ -1,11 +1,48 @@
 package sorting
 
+//Insertion sort works in the way of sorting the cards in hands.
+//i.e. You will take a card, you will just compare it with all the elements to the left of the card
+//i.e. and insert the current card in the correct position, so that all the cards which are positioned at left
+//are moved one step forward to the right.
+//Insertion sort is in-place sorting algorithm which makes sure that
+//after ith iteration, a[0..i-1] are sorted. i.e. i elements are sorted.
+//Works well in best case, since there is no need to swap any element. 0(n)
+//worst case occurs when array is sorted reversely, i.e. 0(n^2)
 type InsertionSort struct{}
 
 func (s *InsertionSort) Sort(arr []int32) []int32 {
+	len := len(arr)
+	if len <= 1 {
+		return arr
+	}
+	for i := 1; i < len; i++ {
+		j := i - 1
+		for j >= 0 && arr[j] > arr[i] {
+			arr[j+1] = arr[j]
+			j = j - 1
+		}
+		arr[j+1] = arr[i]
+	}
 	return arr
 }
 
 func (s *InsertionSort) StableSort(arr []int32) []int32 {
 	return s.Sort(arr)
+}
+
+func recursive(arr []int32, index int) []int32 {
+	if index >= len(arr) {
+		return arr
+	}
+	low := index - 1
+	for low >= 0 && arr[low] > arr[index] {
+		arr[low+1] = arr[low]
+		low = low - 1
+	}
+	arr[low+1] = arr[index]
+	return recursive(arr, index+1) // tail recursion, can be replaced with goto.
+}
+
+func (s *InsertionSort) Recursive(arr []int32) []int32 {
+	return recursive(arr, 1)
 }
--- Algorithms/sorting/merge.go
+++ Algorithms/sorting/merge.go
@@ -3,8 +3,50 @@ package sorting
 //Empty struct just to use the ISort interface..s
 type MergeSort struct{}
 
+//utility function that merges two sorted input arrays and returns the combined sorted output array
+func merge(arr []int32, low, middle, high int) []int32 {
+	firstHalf := [middle - low + 1]int32{}
+	secondHalf := [high - middle]int32{}
+	for i := low; i <= middle; i++ {
+		firstHalf[i] = arr[i]
+	}
+	for i := middle + 1; i <= high; i++ {
+		secondHalf[i] = arr[i]
+	}
+	i, j, k := 0, 0, 0
+	for ; i < len(firstHalf) && j < len(secondHalf); k++ {
+		if firstHalf[i] <= secondHalf[j] {
+			arr[k] = firstHalf[i]
+			i++
+		} else {
+			arr[k] = secondHalf[j]
+			j++
+		}
+	}
+	for ; i < len(firstHalf); i, k = i+1, k+1 {
+		arr[k] = firstHalf[i]
+	}
+	for ; j < len(secondHalf); j, k = j+1, k+1 {
+		arr[k] = secondHalf[j]
+	}
+	return arr
+}
+
+//mergeSort sorts the array by recursively calling itself.
+func mergeSort(arr []int32, low int, high int) []int32 {
+	if low >= high {
+		return arr
+	}
+	//recursively merge the two halves and merge.
+	middle := (low) + (high-low)/2
+	mergeSort(arr, low, middle)
+	mergeSort(arr, middle+1, high)
+	return merge(arr, low, middle, high)
+}
+
 //Implements the ISort interface using Merge Sort algorithm..
 func (s *MergeSort) Sort(arr []int32) []int32 {
+	return mergeSort(arr, 0, len(arr)-1)
 	return arr
 }
 
